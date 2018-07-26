1d77d80d55d4de70a445450557721fb3bbe13ceb
Daniel Askari <daniel.askari@bonnierbroadcasting.com>
2018-07-26 13:19:57 +0200

    Add descriptions for all parts of the schema

--- lib/wc2018_graphql_web/schema.ex
+++ lib/wc2018_graphql_web/schema.ex
@@ -6,9 +6,9 @@ defmodule Wc2018GraphqlWeb.Schema do
   # Absinthe.Schema.lookup_type(Wc2018GraphqlWeb.Schema, "RootQueryType")
   # list_of is handy, meaning this field returns a list of given type.
 
+  @desc "World Cup 2018"
   query do
-    @desc "Contending teams." 
-
+    @desc "Contending teams"
     field :teams, list_of(:team) do
       arg :name, :string
       resolve &Resolvers.Team.team/3
@@ -17,10 +17,11 @@ defmodule Wc2018GraphqlWeb.Schema do
 
   # A new object type (several fields)
   # Absinthe.Schema.lookup_type(Wc2018GraphqlWeb.Schema, "Team")
-  
+
+  @desc "Team representation"
   object :team do
-    field :fifa_code, :string
-    field :name,      :string
-    field :group,     :string
+    field :fifa_code, :string, description: "Three-letter country code"
+    field :name,      :string, description: "Country name"
+    field :group,     :string, description: "Part of this group at start"
   end
 end
